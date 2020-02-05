06446a2e3f9d4d2f6b2e4b8c254fce290daa01fd
David Kern <david33belle@gmail.com>
2020-02-05 18:15:29 +0200

    Complete inputComponent render test for success=false

--- src/Input/index.js
+++ src/Input/index.js
@@ -7,16 +7,41 @@ class InputComponent extends Component {
     }
 
     render(){
+
+        const contents = this.props.success
+            ?
+                null
+            :
+                (
+                    <form className={'form-inline'}>
+                        <input
+                            data-test={'input-box'}
+                            className={'input'}
+                            type={'text'}
+                            placeholder={'enter guess'}
+                        />
+                        <button
+                            data-test={'submit-button'}
+                            className={'btn'}
+                            type={'submit'}
+                            >
+                            Submit
+                        </button>
+                    </form>
+                )
+        ;
+
+
         return (
-            <div>
-                <button />
+            <div data-test={'component-input'}>
+                {contents}
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
-    return {}
+const mapStateToProps = ({ success }) => {
+    return { success };
 }
 
 export default connect(mapStateToProps)(InputComponent);
