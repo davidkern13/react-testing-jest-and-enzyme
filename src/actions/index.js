9f9a55045af6a1a33f115a16342b38e35ee23163
David Kern <david33belle@gmail.com>
2020-02-17 15:00:58 +0200

    fixed error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,31 @@
+import { getLetterMatchCount } from '../helpers';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *     and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
+*/
 export const guessWord = (guessedWord) => {
-    return (dispatch, getState) => {
+  return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
-    };
-}
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import success from './successReducer';
-//import guessedWords from './guessedWordsReducer';
+import guessedWords from './guessedWordsReducer';
+import secretWords from './secretWordReducer';
 
 export default combineReducers({
     success,
-   // guessedWords
+    guessedWords,
+    secretWords
 });
